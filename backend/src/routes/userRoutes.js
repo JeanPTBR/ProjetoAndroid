1ae90e7efc043a823e7eba915a5ce60f55232c22
Jean Carlos <jeanerafael123@gmail.com>
2025-04-29 09:55:03 -0300

    feat: add UpdateUser route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,3 +2,7 @@ import express from "express";
 import { registerUser, loginUser, updateUser, updateUserPassword } from "../controllers/userController.js";
 
 const router = express.Router();
+
+
+router.put("/UpdateUser", updateUser);
+
