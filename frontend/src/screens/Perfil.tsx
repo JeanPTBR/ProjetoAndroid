32fa5776916d2933100bd029376c814128c6b8c6
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 09:06:18 -0300

    feat: add Perfil screen

--- frontend/src/screens/Perfil.tsx
+++ frontend/src/screens/Perfil.tsx
@@ -1,190 +1,157 @@
-import React, { useState, useContext } from "react";
-import { View, Text, Image, StyleSheet, ScrollView , Alert } from "react-native";
-import { TextInput } from 'react-native-paper';
-import { Button } from "react-native-paper";
-import { RouteProp } from "@react-navigation/native";
-import { RootStackParamList } from "../types";
-import { ProfileContext } from "./contexts/ProfileContext";
+import React, { useEffect, useContext, useState } from "react";
+import { View, Text, Image } from "react-native";
+import { Button } from "react-native-paper"
+import { ProfileContext } from "../contexts/ProfileContext";
+import { RouteProp, useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../types';
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MaterialCommunityIcons, Ionicons, Feather } from '@expo/vector-icons';
+import { styles } from "../styles/Perfil.styles";
+import { useCart } from '../contexts/CartContext';
 
-type ProfileScreenRouteProp = RouteProp<RootStackParamList, "Perfil">;
+type PerfilScreenRouteProp = RouteProp<RootStackParamList, 'Perfil'>;
+type PerfilScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Perfil'>;
 
-const Perfil = ({ route }: { route: ProfileScreenRouteProp }) => {
-  //const { nome: initialNome, email: initialEmail } = route.params;
-  const { nome, email, setNome, setEmail } = useContext(ProfileContext);
-  const [isEditing, setIsEditing] = useState(false);
+const Perfil = ({ route }: { route: PerfilScreenRouteProp }) => {
 
-  const handleEditPress = () => {
-    setIsEditing(true);
-  };
+  const navigation = useNavigation<PerfilScreenNavigationProp>();
 
-  const handleSavePress = () => {
-    if (nome && email) {
-      setIsEditing(false);
-      Alert.alert('Sucesso', 'Nome e email salvos com sucesso ✔');
+  const { cart } = useCart();
+  const { nome: nomeAtual, email: emailAtual } = route.params;
+
+  const [nome] = useState(nomeAtual);
+  const [email] = useState(emailAtual);
+  const { imagemPerfil, setImagemPerfil } = useContext(ProfileContext);
+  const total = cart.reduce((sum, item) => sum + item.preco * item.quantidade, 0);
+  const data = new Date().toLocaleDateString();
+  const [selectedItems] = useState<number[]>([]);
+
+  useEffect(() => {
+    carregarImagem();
+  }, []);
+
+  const carregarImagem = async () => {
+    const uriSalva = await AsyncStorage.getItem('imagemPerfil');
+    if (uriSalva) {
+
+      const fileExistsResult = await fileExists(uriSalva);
+      if (fileExistsResult) {
+        setImagemPerfil(uriSalva);
+      } else {
+        await AsyncStorage.removeItem('imagemPerfil');
+        setImagemPerfil(null);
+      }
     } else {
-      Alert.alert('Erro', 'Nome e email não puderam ser salvos.');
+      setImagemPerfil(null);
     }
   };
 
-  const handleCancelPress = () => {
-    //setNome(initialNome);
-    //setEmail(initialEmail);
-    setIsEditing(false);
+  const fileExists = async (uri: string): Promise<boolean> => {
+    try {
+      const response = await fetch(uri);
+      return response.status === 200;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const handleCameraPress = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      const uri = result.assets[0].uri;
+      await AsyncStorage.setItem('imagemPerfil', uri);
+      setImagemPerfil(uri);
+    }
+  };
+
+  const goRedefinedProfile = async () => {
+    navigation.navigate('RedefinirPerfil', { nome: nome, email: email });
+  };
+
+  const goMyOrders = async () => {
+
+    const selectedProducts = cart.filter(item => selectedItems.includes(item.id));
+    const totalSelecionado = selectedProducts.reduce((acc, item) => acc + item.preco * item.quantidade, 0);
+    const dataCompra = new Date().toISOString();
+
+    navigation.navigate('Pedidos', {
+      nome,
+      email,
+      produtos: selectedProducts,
+      total: totalSelecionado,
+      data: dataCompra
+    });
+  };
+
+  const goMyCart = async () => {
+    navigation.navigate('Carrinho', { nome: nome, email: email, produtos: [], total, data })
+  }
+
+  const handleLogout = () => {
+    navigation.navigate('Introducao');
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+    <View style={styles.body}>
+      <View style={styles.container1}>
+        <View style={styles.logoutButton}>
+          <Button onPress={handleLogout}>
+            <Text style={styles.logoutButtonText}>Sair</Text>
+          </Button>
+        </View>
+        <Image source={require('../assets/Logo.png')} style={styles.logo} />
+      </View>
+      <Ionicons
+        style={styles.iconVoltar}
+        onPress={() => navigation.navigate('Home', { nome, email })}
+        name="arrow-back"
+        size={30}
+        color="black" 
+      />
       <View style={styles.container}>
-        <Image source={require("../../assets/perfil.png")} style={styles.image} />
-        <Text style={styles.title}>Perfil do usuário</Text>
 
-        {isEditing ? (
-          <>
-            <TextInput
-              label="Nome"
-              style={[styles.text, styles.input]}
-              value={nome}
-              onChangeText={setNome}
-              placeholder="Digite seu nome"
-            />
-            <TextInput
-              label="Email"
-              style={[styles.text, styles.input]}
-              value={email}
-              onChangeText={setEmail}
-              placeholder="Digite seu email"
-              keyboardType="email-address"
-            />
-          </>
+        {imagemPerfil ? (
+          <Image source={{ uri: imagemPerfil }} style={styles.image} />
         ) : (
-          <>
-            <Text style={styles.text}>Nome: {nome}</Text>
-            <View style={styles.separator} />
-            <Text style={styles.text}>Email: {email}</Text>
-            <View style={styles.separator} />
-          </>
+          <Image source={require("../assets/Perfil.webp")} style={styles.image} />
         )}
-
-        {isEditing ? (
-          <View style={styles.buttonContainer}>
-            <Button
-              mode="contained"
-              onPress={handleSavePress}
-              style={styles.saveButton}>
-                <Text style={styles.textSaveButton}>
-                  Salvar
-                </Text>
-            </Button>
-            <Button
-              mode="outlined"
-              onPress={handleCancelPress}
-              style={styles.cancelButton}>
-                <Text style={styles.textCancelButton}>
-                  Cancelar
-                </Text>
-            </Button>
-          </View>
-        ) : (
-          <Button
-            mode="contained"
-            onPress={handleEditPress}
-            style={styles.button}>
-            <Text style={styles.textbutton}>
-              Editar
+        <MaterialCommunityIcons
+          name="camera"
+          size={40}
+          color="#000"
+          style={styles.camera}
+          onPress={handleCameraPress}
+        />
+        <View style={styles.botao}>
+          <Feather name="edit-2" size={30} color="white" onPress={goRedefinedProfile} />
+        </View>
+        <Text style={styles.title}>Dados do usuário</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.dados}>Nome: {nome}</Text>
+          <Text style={styles.dados}>Email: {email}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button style={styles.buttonMyOrders} onPress={goMyOrders}>
+            <Text style={styles.textButtonMyOrders}>
+              Meus Pedidos
             </Text>
           </Button>
-        )}
+          <Button style={styles.buttonMyCart} onPress={goMyCart}>
+            <Text style={styles.textButtonMyCart}>
+              Meu carrinho
+            </Text>
+          </Button>
+        </View>
       </View>
-    </ScrollView>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollViewContent: {
-    flexGrow: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingVertical: 20,
-    backgroundColor: "rgba(194, 194, 194, 0.904)",
-  },
-  container: {
-    width: "87%",
-    height: 480,
-    backgroundColor: "rgb(255, 255, 255)",
-    borderRadius: 10,
-    padding: 20,
-    alignItems: "center",
-    position: 'absolute',
-    top: '9%',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
-    color: "rgb(0, 0, 0)",
-  },
-  text: {
-    fontSize: 15,
-    marginBottom: 10,
-    marginTop: 10,
-    color: "rgb(0, 0, 0)",
-  },
-  input: {
-    width: "100%",
-    backgroundColor: "rgba(228, 228, 228, 0.959)",
-    color: "rgb(0, 0, 0)",
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    marginBottom: 20,
-  },
-  separator: {
-    width: "100%",
-    height: 1,
-    marginTop: 5,
-    backgroundColor: "black",
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-    marginTop: 20,
-  },
-  button: {
-    backgroundColor: "rgb(39, 177, 241)",
-    width: "70%",
-    height: 40,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  textbutton:{
-    fontSize: 17,
-    color: "rgb(255, 255, 255)",
-  },
-  saveButton: {
-    width: "50%",
-    backgroundColor: "rgba(49, 109, 223, 0.863)",
-    borderRadius: 10,
-  },
-  cancelButton: {
-    width: "50%",
-    backgroundColor: "rgba(226, 61, 61, 0.911)",
-    marginLeft: 10,
-    borderRadius: 10,
-  },
-  textSaveButton:{
-    fontSize: 17,
-    color: "rgb(255, 255, 255)",
-  },
-  textCancelButton:{
-    fontSize: 17,
-    color: "rgb(255, 255, 255)",
-  },
-});
 
 export default Perfil;
