86bd157e303f8bd369d2f619546cddf4396f9b92
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 08:32:48 -0300

    chore: add database port

--- backend/src/config/connectionDataBase.js
+++ backend/src/config/connectionDataBase.js
@@ -6,11 +6,12 @@ const data_base = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 data_base.connect(err => {
-  if (err) console.log("Erro ao conectar ao MySQL");
-  else console.log("Conectado ao MySQL!");
+  if (err) console.log("MySQL\nStatus: Erro ao conectar");
+  else console.log("MySQL\nStatus: Conectado");
 });
 
 export default data_base;
