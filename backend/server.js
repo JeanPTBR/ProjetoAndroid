39ecf75167f325af1daa50f640d3502eca5497da
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 08:36:05 -0300

    feat: add new route in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import userRoutes from "./src/routes/userRoutes.js";
 import productRoutes from "./src/routes/productRoutes.js";
+import pagamentoRoute from "./src/routes/efiBankRoute.js"; 
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -12,10 +16,12 @@ app.use(bodyParser.json());
 app.use("/", userRoutes);
 app.use("/", productRoutes);
 
+app.use('/api/pagamento', pagamentoRoute);
+
 app.get("/", (req, res) => {
   res.send("Seja bem-vindo à API!");
 });
 
 app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+  console.log("Servidor\nStatus: rodando");
 });
