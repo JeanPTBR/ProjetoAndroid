9417430f42143deadc1874b3542ff7614f8d86ea
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 09:05:43 -0300

    feat: add Cadastro screen

--- frontend/src/screens/Cadastro.tsx
+++ frontend/src/screens/Cadastro.tsx
@@ -1,118 +1,131 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Alert, View, StyleSheet } from 'react-native';
-import { TextInput, Button, Text } from 'react-native-paper';
+import { Alert, Image, View, Pressable } from 'react-native';
+import { Text, TextInput, Button } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
+import { styles } from '../styles/Cadastro.styles';
 
 type CadastroScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Cadastro'>;
 
 const Cadastro = () => {
 
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-
   const navigation = useNavigation<CadastroScreenNavigationProp>();
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isHoveredButtonRegister, setIsHoveredButtonRegister] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const realizarCadastro = async () => {
     if (!nome || !email || !senha) {
-      Alert.alert("Ops ❗", "Por favor, preencha todos os campos.");
+      Alert.alert("Ops!", "Por favor, preencha todos os campos.");
       return;
     }
     try {
-      const response = await axios.post("http://192.168.1.19:5000/register", {
+      const response = await axios.post("http://192.168.1.19:3000/register", {
         nome,
         email,
         senha
       });
 
-      setNome("");
-      setEmail("");
-      setSenha("");
+      Alert.alert("Cadastro feito com sucesso ✔", response.data.message);
 
-      Alert.alert("Sucesso ✔", response.data.message);
+      setNome('');
+      setEmail('');
+      setSenha('');
+
+      navigation.navigate('Home', { nome: nome, email: email });
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
         if (error.response?.data.error === "Este email já esta cadastrado!") {
-          Alert.alert("Erro ❗", "Este email já está cadastrado.");
+          Alert.alert("Erro!", "Este email já está cadastrado.");
         } else {
-          Alert.alert("Erro ❗", error.response?.data.message || "Voce já está cadastrado.");
+          Alert.alert("Erro!", error.response?.data.message || "Voce já está cadastrado.");
         }
       } else {
-        Alert.alert("Erro desconhecido", "Ocorreu um erro inesperado.");
+        Alert.alert("Erro desconhecido ☹", "Ocorreu um erro inesperado.");
+        console.log(error);
       }
     }
   };
 
   return (
     <View style={styles.body}>
+      <Ionicons
+        style={styles.iconVoltar}
+        onPress={() => navigation.navigate('Introducao')}
+        name="arrow-back"
+        size={30}
+        color="black" />
+      <View style={styles.container1}>
+        <Image source={require('../assets/Logo.png')} style={styles.logo} />
+      </View>
       <View style={styles.container}>
-        <Text style={styles.title}>Formulário de cadastro</Text>
-
+        <Text style={styles.title}>
+          Formulário de cadastro
+        </Text>
         <TextInput
-          label="Nome"
+          label={<Text style={styles.labelInputSenha}>Nome</Text>}
+          style={styles.inputNome}
           placeholder='Digite seu nome'
+          placeholderTextColor="#3b3b3b"
           value={nome}
           onChangeText={setNome}
-          style={styles.inputNome}
-          mode="outlined"
+          activeUnderlineColor="rgb(255, 0, 0)"
         />
-
         <TextInput
-          label="Email"
-          placeholder='Digite seu email'
+          label={<Text style={styles.labelInputSenha}>Email</Text>}
+          style={styles.inputEmail}
+          placeholder='Seuemail@gmail.com'
+          placeholderTextColor="#3b3b3b"
           value={email}
           onChangeText={setEmail}
-          style={styles.inputEmail}
-          mode="outlined"
           keyboardType="email-address"
+          activeUnderlineColor="rgb(102, 255, 0)"
         />
-
         <TextInput
-          label="Senha"
-          placeholder='Digite sua senha (4 dígitos)'
+          label={<Text style={styles.labelInputSenha}>Senha</Text>}
+          style={styles.inputSenha}
+          placeholder={'Digite sua senha'}
+          placeholderTextColor="#3b3b3b"
+          secureTextEntry={!showPassword}
           value={senha}
           onChangeText={setSenha}
-          style={styles.inputSenha}
-          mode="outlined"
-          maxLength={4}
-          secureTextEntry={true}
-          keyboardType="numeric"
+          activeUnderlineColor="rgb(0, 47, 255)"
         />
-
-        <Button mode="contained" onPress={realizarCadastro} style={styles.buttonRegister} >
-          <Text style={styles.Textbutton} >
-            Cadastrar
-          </Text>
-        </Button>
+        <Icon
+          name={showPassword ? "visibility-off" : "visibility"}
+          size={24}
+          onPress={togglePasswordVisibility}
+          style={styles.icon}
+        />
+        <Pressable
+          onPress={realizarCadastro}
+          onPressIn={() => setIsHoveredButtonRegister(true)}
+          onPressOut={() => setIsHoveredButtonRegister(false)} >
+          <Button
+            style={[
+              styles.buttonBase,
+              isHoveredButtonRegister ? styles.buttonRegisterhover : styles.buttonRegister
+            ]}>
+            <Text style={isHoveredButtonRegister ? styles.textButtonRegisterHover : styles.textButtonRegister}>
+              Cadastrar
+            </Text>
+          </Button>
+        </Pressable>
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-
-  body: { flex: 1, justifyContent: "center", padding: 20 },
-
-  container: { justifyContent: "center", padding: 20, backgroundColor: "rgb(172, 172, 172)", marginBottom: 10, borderRadius: 10, width: "100%", height: 400 },
-
-  title: {
-    fontSize: 24, fontWeight: "bold", textAlign: "center", marginBottom: 20, fontFamily: "Arial",
-    color: 'rgb(252, 252, 252)'
-  },
-
-  inputNome: { marginBottom: 10, borderColor: "rgb(2, 2, 2)", fontFamily: "Roboto", width: "100%", height: 40 },
-
-  inputEmail: { marginBottom: 10, borderColor: "rgb(2, 2, 2)", fontFamily: "Roboto", width: "100%", height: 40 },
-
-  inputSenha: { marginBottom: 10, borderColor: "rgb(2, 2, 2)", fontFamily: "Roboto", width: "100%", height: 40 },
-
-  buttonRegister: { backgroundColor: "rgb(129, 129, 129)", marginTop: 30, borderRadius: 10, marginBottom: 10 },
-
-  Textbutton: { fontSize: 19, fontFamily: "Roboto", color: 'rgb(252, 252, 252)' },
-
-});
 
 export default Cadastro;
