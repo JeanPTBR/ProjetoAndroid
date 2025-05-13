2b801aa37fdf6c1c59d254bfe9d7aded070dd3ac
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 09:03:33 -0300

    feat: add Home screen

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,52 +1,136 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, FlatList, ActivityIndicator, TextInput, Alert, BackHandler }
+  from 'react-native';
 import { Button } from 'react-native-paper';
-import { useNavigation, RouteProp } from '@react-navigation/native';
+import { useNavigation, RouteProp, useFocusEffect } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
+import { Product, useCart } from '../contexts/CartContext';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { styles } from '../styles/Home.styles';
 
 type HomeScreenRouteProp = RouteProp<RootStackParamList, 'Home'>;
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
-const Home = ({ route }: { route: HomeScreenRouteProp }) => {
+const Home = React.memo(({ route }: { route: HomeScreenRouteProp }) => {
+
   const { nome, email } = route.params;
   const navigation = useNavigation<HomeScreenNavigationProp>();
+  const [dados, setDados] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  const { addToCart } = useCart();
+  const { cart } = useCart();
+
+  useEffect(() => {
+    setLoading(true);
+
+    const url = searchTerm.trim()
+      ? `http://192.168.1.19:3000/dadosDosProdutos?nome=${encodeURIComponent(searchTerm)}`
+      : 'http://192.168.1.19:3000/dadosDosProdutos';
+
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        if (json.length === 0) {
+          Alert.alert('Ops!', 'Produto não encontrado.');
+        }
+        setDados(json);
+      })
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false));
+  }, [searchTerm]);
+
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        return true;
+      };
+
+      const backHandler = BackHandler.addEventListener(
+        'hardwareBackPress',
+        onBackPress
+      );
+
+      return () => backHandler.remove();
+    }, [])
+  );
+
+  
   return (
     <View style={styles.body}>
-      <View style={styles.buttonProfile}>
-        <Button onPress={() => navigation.navigate('Perfil', { nome, email })}>
-          <Image source={require('../../assets/meuperfil.png')} style={styles.meuPerfil}/>
-        </Button>
+      <View style={styles.menu}>
+        <View style={styles.ContainerPerfil}>
+          <MaterialCommunityIcons
+            name="account"
+            size={50}
+            color="#000000"
+            style={styles.meuPerfil}
+            onPress={() => navigation.navigate('Perfil', { nome: nome, email: email })}
+          />
+          <MaterialCommunityIcons
+            onPress={() => navigation.navigate('Avaliacao', { nome: nome, email: email })}
+            name="star"
+            size={40}
+            color="#000000"
+            style={styles.star}
+          />
+        </View>
       </View>
-      <Text style={styles.label}>Perfil</Text>
+      <View style={styles.header}>
+        <Text style={styles.saudacao}>Olá {nome}!</Text>
+      </View>
+      <View style={styles.container1}>
+        <Image source={require('../assets/Logo.png')} style={styles.logo} />
+      </View>
+      <TextInput
+        placeholder='Pesquise aqui'
+        placeholderTextColor="#3b3b3b"
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        style={{
+          backgroundColor: '#fff',
+          padding: 10,
+          borderRadius: 10,
+          marginBottom: 10,
+        }}
+      />
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={dados}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+
+            <View style={styles.productCard}>
+
+              <Image source={{ uri: item.imagemUrl }} style={styles.image} />
+
+              <Text style={styles.productName}>{item.nome}</Text>
+
+              <Button
+                mode="outlined"
+                onPress={() => addToCart(item, true)}
+                style={styles.addButton}>
+                <Text style={styles.TextaddButton}>Adicionar ao carrinho</Text>
+              </Button>
+
+              <Button
+                mode="outlined"
+                onPress={() => navigation.navigate("Produto", { nome: nome, email: email, produto: item })}
+                style={styles.verProduto}>
+                <Text style={styles.TextVerProduto} >Ver sobre</Text>
+              </Button>
+            </View>
+          )}
+          extraData={cart}
+        />
+      )}
     </View>
   );
-};
-
-const styles = StyleSheet.create({
-  body:{
-    backgroundColor: "rgb(231, 231, 231)",
-    height: '100%',
-  },
-  label:{
-    color: "rgb(245, 245, 245)",
-    fontSize: 16,
-    position: 'absolute',
-    top: '9%',
-    left: '5%',
-  },
-  meuPerfil:{
-    width: 50,
-    height: 50,
-    marginLeft: '5%',
-  },
-  buttonProfile: {
-    backgroundColor: "rgb(158, 158, 158)",
-    borderRadius: 10,
-    height: 100,
-    width: '20%',
-  },
 });
 
 export default Home;
