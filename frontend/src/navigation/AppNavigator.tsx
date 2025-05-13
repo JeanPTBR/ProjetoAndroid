3ebd7ed62d6c5b0d18418b255e8aea8b0fc28ac7
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 09:34:33 -0300

    feat: add App Navigator

--- frontend/src/navigation/AppNavigator.tsx
+++ frontend/src/navigation/AppNavigator.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParamList } from '../types';
+import { CartProvider } from '../contexts/CartContext';
+
+// import das navegaçoes
 import Introducao from '../screens/Introducao';
 import Login from '../screens/Login';
+import RedefinirSenha from '../screens/RedefinirSenha';
 import Cadastro from '../screens/Cadastro';
 import Home from '../screens/Home';
+import Carrinho from '../screens/Carrinho';
+import Pedidos from '../screens/Pedidos';
 import Perfil from '../screens/Perfil';
-import { RootStackParamList } from '../types';
+import RedefinirPerfil from '../screens/RedefinirPerfil';
+import Pagamento from '../screens/Pagamento';
+import Produto from '../screens/Produto';
+import Avaliacao from '../screens/Avaliacao';
+import FaleConosco from '../screens/FaleConosco';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Introducao">
-        <Stack.Screen name="Introducao" component={Introducao} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Perfil" component={Perfil} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Introducao">
+          <Stack.Screen name="Introducao" component={Introducao} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+          <Stack.Screen name="RedefinirSenha" component={RedefinirSenha} options={{ headerShown: false }} />
+          <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Carrinho" component={Carrinho} options={{ headerShown: false }} />
+          <Stack.Screen name='Pedidos' component={Pedidos} options={{ headerShown: false }} />
+          <Stack.Screen name="Pagamento" component={Pagamento} options={{ headerShown: false }}/>
+          <Stack.Screen name="Perfil" component={Perfil} options={{ headerShown: false }} />
+          <Stack.Screen name="RedefinirPerfil" component={RedefinirPerfil} options={{ headerShown: false }} />
+          <Stack.Screen name="Produto" component={Produto} options={{ headerShown: false }} />
+          <Stack.Screen name="Avaliacao" component={Avaliacao} options={{ headerShown: false }}/>
+          <Stack.Screen name="FaleConosco" component={FaleConosco} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 };
 
