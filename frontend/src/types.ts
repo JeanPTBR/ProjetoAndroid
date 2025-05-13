43360b58e5f4793736f4f713a6a450ce26627928
JEAN CARLOS PENHA DA CONCEICAO <202308342665@alunos.estacio.br>
2025-05-13 08:55:59 -0300

    feat: add TypeScript types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,9 +1,55 @@
+import { Product } from "./contexts/CartContext";
+
 export type RootStackParamList = {
     Introducao: undefined;
     Login: undefined;
-    Cadastro: undefined; 
-    Home: { nome: string; email: string };
-    Perfil: { nome: string; email: string };   
-    
+    Cadastro: undefined;
+    RedefinirSenha: {
+        email: string;
+    };
+    Avaliacao: {
+        nome: string;
+        email: string;    
+    };
+    Produto: {
+        produto: Product;
+        nome: string;
+        email: string;
+    };
+    Home: {
+        nome: string;
+        email: string;
+    };
+    Carrinho: {
+        nome: string;
+        email: string;
+        produtos: Product[];
+        total: number;
+        data: string;
+    };
+    Pedidos: {
+        nome: string;
+        email: string;
+        produtos: Product[];
+        total: number;
+        data: string;
+    };
+    Perfil: {
+        nome: string;
+        email: string;
+    };
+    Pagamento: {
+        nome: string;
+        email: string;
+        produtos: Product[];
+        total: number;
+    };
+    RedefinirPerfil: {
+        nome: string;
+        email: string;
+    },
+    FaleConosco: {
+        nome: string;
+        email: string;
+    },
 };
-  
